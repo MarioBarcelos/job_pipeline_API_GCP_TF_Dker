fa273a3fc0a727e7a99c5bd1a817280f112a1b55
MarioBarcelos <mariosbpimentel@gmail.com>
2023-08-24 00:04:37 -0300

    creating bucket

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # job_pipeline_API_GCP_TF_Dker
-# job_pipeline_API_GCP_TF_Dker
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" { 
+  type  = string
+  description = "O Id do projecto no GCP"
+}
+
+variable " region" {
+    type = string
+    default = "Google Cloud location of resources"
+}
